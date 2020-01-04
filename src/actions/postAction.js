ccfcefdf9deeb6a9ed9fd9d1d4af3db6fec1b994
Leo Wang <bluebreeze0812@outlook.com>
2020-01-05 02:20:44 +0800

    convert add post component to call redux instead

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -1,8 +1,26 @@
-import {FETCH_POST, NEW_POST, FETCH_POSTS} from './types'
+import {NEW_POST, FETCH_POSTS} from './types'
 
 export const fetchPosts = () => dispatch => {
-        fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json()).then(data => dispatch({
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => dispatch({
             type: FETCH_POSTS,
             payload: data
+        })
+    )
+}
+
+export const createPost = (postData) => dispatch => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+        .then(res => res.json())
+        .then(data => dispatch({
+            type: NEW_POST,
+            payload: data
         }))
-    }
+}
